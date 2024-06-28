f0783bb2174bcf37fb287a81fb434f45624c4f0a
Brayam Programador <brateomx@gmail.com>
2024-06-28 12:36:02 -0600

    adecuacion de vista sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -24,37 +24,45 @@
 
 <style>
     .sidebar {
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 250px; /* Ancho del sidebar */
-    background-color: #343a40; /* Color de fondo del sidebar */
-    padding-top: 1rem; /* Espaciado superior */
-}
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 250px;
+        /* Ancho del sidebar */
+        background-color: #343a40;
+        /* Color de fondo del sidebar */
+        padding-top: 1rem;
+        /* Espaciado superior */
+    }
 
-.sidebar ul.navbar-nav {
-    padding-left: 0; /* Elimina el padding izquierdo de la lista */
-}
+    .sidebar ul.navbar-nav {
+        padding-left: 0;
+        /* Elimina el padding izquierdo de la lista */
+    }
 
-.sidebar .nav-link {
-    color: #dee2e6; /* Color del texto */
-}
+    .sidebar .nav-link {
+        color: #dee2e6;
+        /* Color del texto */
+    }
 
-.sidebar .nav-link:hover {
-    background-color: #495057; /* Color de fondo al pasar el mouse */
-}
+    .sidebar .nav-link:hover {
+        background-color: #495057;
+        /* Color de fondo al pasar el mouse */
+    }
 
-.sidebar .active {
-    background-color: #6c757d; /* Color de fondo del elemento activo */
-}
+    .sidebar .active {
+        background-color: #6c757d;
+        /* Color de fondo del elemento activo */
+    }
 
-.sidebar .bi {
-    margin-right: 0.5rem; /* Espacio entre el ícono y el texto */
-}
+    .sidebar .bi {
+        margin-right: 0.5rem;
+        /* Espacio entre el ícono y el texto */
+    }
 
-.sidebar span {
-    font-size: 0.9rem; /* Tamaño de la fuente del texto */
-}
-
+    .sidebar span {
+        font-size: 0.9rem;
+        /* Tamaño de la fuente del texto */
+    }
 </style>
