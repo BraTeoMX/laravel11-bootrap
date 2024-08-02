ecbc5bc5e0ca6d8a04a4f71bb09bb4d90dce9818
Brayam Programador <brateomx@gmail.com>
2024-08-02 12:33:29 -0600

    adaptando un poco mejor el codigo:

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
   <div class="container">
-      @if (session('status'))
-          <div class="alert alert-success" role="alert">
-              {{ session('status') }}
-          </div>
-      @endif
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
           <div class="card">
-            <div class="card-header">{{ __('Dashboard') }}</div>
-            <div class="card-body">
+            <div class="card-header">
+              <h4>Dato del dashboard</h4>
             </div>
-            <div class="row">
+            <div class="card-body">
               <h1> Inicio del sitio </h1>
             </div>
           </div>
